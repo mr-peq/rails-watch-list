392b968e8f1505750091f348574bbc8c9051a4cf
Theodore Saulnier <theodore1712@yahoo.fr>
2023-11-17 11:07:01 +0100

    Added root

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  get 'lists', to: 'lists#index'
+  root to: 'lists#index'
   get 'lists/new', to: 'lists#new'
   get 'lists/:id', to: 'lists#show', as: 'list'
   post 'lists', to: 'lists#create'
